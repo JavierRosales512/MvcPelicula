48ae7ad8747775b91613093a0e5436e51839e634
JavierRosales512 <javier.rosales597@outlook.com>
2024-09-03 11:39:24 -0600

    Agregar archivos de proyecto.

---
+++ MvcPelicula/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcPelicula.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

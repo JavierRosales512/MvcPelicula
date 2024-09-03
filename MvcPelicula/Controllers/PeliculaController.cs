5811e69af1e9889167f4456d8735418f575bbe2f
JavierRosales512 <javier.rosales597@outlook.com>
2024-09-03 17:16:34 -0600

    Actualización de la aplicación MVC:
    - Se agrego una vista, un controlador para la vista.

---
+++ MvcPelicula/Controllers/PeliculaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+namespace MvcPelicula.Controllers
+{
+    public class PeliculaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
